cb108e231b1b229c3c81a4728fe828f53a9bfb3b
nuochong <nuo.chong@163.com>
2019-11-02 13:24:22 +0800

    update

--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>
-    <card :text="title"></card>
+  <div class="my-container">
+    <div class="my-wrap">
+      <img class="my-empty" src="/static/images/empty.png" />
+      <article-write-btn></article-write-btn>
+      <button class="btn btn-success">
+        <i class="iconfont iconweixin"></i>
+        微信一键登录</button>
+      <button class="btn btn-primary">已有简书账号登录</button>
+    </div>
   </div>
 </template>
 
 <script>
-import card from '@/components/card';
+import articleWriteBtn from '@/components/article-write-btn';
 
 export default {
   data() {
@@ -14,19 +21,42 @@ export default {
     };
   },
   components: {
-    card
+    articleWriteBtn
   },
   methods: {
-    sample() {
-
-    }
+    sample() {}
   },
-  created() {
-
-  }
+  created() {}
 };
 </script>
 
 <style lang="scss" scoped>
-
+.my-container {
+  text-align: center;
+  .my-wrap {
+    padding: 0px 15px;
+  }
+  .my-empty {
+    height: 240px;
+    width: 240px;
+    margin-top: 80px;
+  }
+  .btn {
+    margin: 15px;
+  }
+  .btn-primary {
+    background-color: #f8f8f8;
+    &:hover {
+      color: rgba(0, 0, 0, 0.6);
+      background-color: #dedede;
+    }
+  }
+  .btn-success {
+    background-color: #07c160;
+    color:white;
+    &.button-hover {
+      background-color: #06ad56!important;
+    }
+  }
+}
 </style>
