3dca80fda3fe33da65483c19d02a8b379f511a84
nuochong <nuo.chong@163.com>
2019-11-02 18:15:18 +0800

    update

---
+++ src/pages/award/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <card :text="title"></card>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card';
+
+export default {
+  data() {
+    return {
+      title: 'components'
+    };
+  },
+  components: {
+    card
+  },
+  methods: {
+    sample() {
+
+    }
+  },
+  created() {
+
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
