87c33163eed62e21be42742e0c3f77b19164c728
nuochong <nuo.chong@163.com>
2019-11-06 10:30:07 +0800

    update

--- src/components/article-write-btn.vue
+++ src/components/article-write-btn.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="article-write-btn-wrap">
-    <button class="article-write-btn">
+    <button class="article-write-btn" :class="{'article-write-btn-hidden':isHidden}">
       <i class="iconfont iconadd"></i>
       写文章
     </button>
@@ -8,12 +8,58 @@
 </template>
 
 <script>
+import articleWriteBtn from '@/components/article-write-btn';
+
 export default {
-  props: ['text']
+  props: ['text'],
+  data() {
+    return {
+      title: 'components',
+      scrollTop: 0,
+      isHidden: false
+    };
+  },
+  components: {},
+  methods: {},
+  created() {},
+  //监听屏幕滚动 判断上下滚动
+  onPageScroll: function(ev) {
+    var _this = this;
+    console.log('ev', ev);
+    //当滚动的top值最大或最小时，为什么要做这一步是因为在手机实测小程序的时候会发生滚动条回弹，所以为了处理回弹，设置默认最大最小值
+    if (ev.scrollTop <= 0) {
+      ev.scrollTop = 0;
+    } else if (ev.scrollTop > wx.getSystemInfoSync().windowHeight) {
+      ev.scrollTop = wx.getSystemInfoSync().windowHeight;
+    }
+    //判断浏览器滚动条上下滚动
+    if (ev.scrollTop > this.scrollTop || ev.scrollTop == wx.getSystemInfoSync().windowHeight) {
+      console.log('向下滚动');
+      this.isHidden = true;
+      //向下滚动
+    } else {
+      console.log('向上滚动');
+      this.isHidden = false;
+      //向上滚动
+    }
+    //给scrollTop重新赋值
+    setTimeout(function() {
+      _this.scrollTop = ev.scrollTop;
+    }, 0);
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+.animation {
+  -webkit-transition-duration: 0.5s;
+  -o-transition-duration: 0.5s;
+  transition-duration: 0.5s;
+}
+.article-write-btn-hidden {
+  bottom: -80px !important;
+  @extend .animation;
+}
 .article-write-btn {
   height: 40px;
   line-height: 40px;
@@ -33,5 +79,6 @@ export default {
   -moz-box-shadow: 5px 5px 15px #6e6e6e;
   -webkit-box-shadow: 5px 5px 15px #6e6e6e;
   box-shadow: 5px 5px 15px #6e6e6e;
+  @extend .animation;
 }
 </style>
